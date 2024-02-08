33689615276b0de21ba802daf7799707eafe65d3
Johnson1255 <johnson.chen@upb.edu.co>
2024-02-07 23:45:45 -0500

    Incorporacion de libreria algs4

---
+++ Ejemplo.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ejemplo {
+
+    public static void main(String[] args) {
+        int a = StdIn.readInt();
+        StdOut.println("Hola Mundo " + a);
+    }
+}
