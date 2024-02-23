0b81dfe0a03504a677fcb206c5d1c1161d7595f5
Johnson1255 <johnson.chen@upb.edu.co>
2024-02-22 23:23:13 -0500

    Implementacion de metodos

--- TallerADT.java
+++ TallerADT.java
@@ -92,4 +92,78 @@ public class TallerADT {
             return "Fila de Personas = " + personas;
         }
     }
+
+    class SimuladorFilas{
+        private int numAgentes;
+        private Fila fila;
+        private Agente[] agentes;
+        private int tiempoMaximo;
+        private double promedioOcupacion;
+        private double promedioEspera;
+
+        public SimuladorFilas(int numAgentes, Persona[] personas, int tiempoMaximo){
+            this.numAgentes = numAgentes;
+            this.fila = new Fila();
+            this.agentes = new Agente[numAgentes];
+            this.tiempoMaximo = tiempoMaximo;
+            this.promedioOcupacion = 0.0;
+            this.promedioEspera = 0.0;
+
+            for(int i = 0; i < numAgentes; i++){
+                agentes[i] = new Agente();
+            }
+            
+            for(Persona persona: personas){
+                fila.agregarPersona(persona);
+            }
+            
+        }
+
+        //Correr la simulacion
+        public void correrSimulacion(){
+            int tiempo = 0;
+            int totalTiempoEspera = 0;
+            int totalTiempoOcupado = 0;
+            int personasAtendidas = 0;
+
+            while(tiempo < tiempoMaximo){
+
+                for(Agente agente : agentes){
+
+                    if(agente.getTiempoOcupado() == 0){
+                        Persona persona = fila.siguientePersona();
+
+                        if (persona != null){
+                            persona.setTiempoEspera(tiempo - persona.getTiempoLlegada());
+                            totalTiempoEspera += persona.getTiempoEspera();
+                            agente.aumentarTiempoOcupado(persona.getTiempoServicio());
+                            personasAtendidas++;
+                        }
+                    } else {
+                        agente.aumentarTiempoOcupado(-1);
+                        totalTiempoOcupado++;
+                    }
+                }
+                tiempo++;
+            }
+
+            //Promedio de ocupacion
+            promedioOcupacion = (totalTiempoOcupado / (double) (tiempoMaximo * numAgentes)) * 100;
+
+            //Tiempo Promedio de Atencion a las personas
+            if(personasAtendidas > 0){
+                promedioEspera = totalTiempoEspera / (double) personasAtendidas;
+            }
+
+        }
+
+        public double getPromedioOcupacion(){
+            return promedioOcupacion;
+        }
+
+        public double getPromedioEspera(){
+            return promedioEspera;
+        }
+
+    }
 }
