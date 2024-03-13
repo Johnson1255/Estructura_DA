384c606d1c9dfda5d5044f3e44a8e4c16fbc80cb
Johnson1255 <johnson.chen@upb.edu.co>
2024-03-12 23:20:53 -0500

    Documento Base v2.0

--- Taller2.java
+++ Taller2.java
@@ -3,20 +3,38 @@ import java.util.List;
 import java.util.Random;
 
 
-public class Taller2 {
-    
-    class Punto2D{
-        double x;
-        double y;
-    
-        public Punto2D(double x, double y){
-            this.x = x;
-            this.y = y;
-        }
+class Punto2D {
+    double x;
+    double y;
 
-        public double distancia(Punto2D otroPunto) {
-            return Math.sqrt(Math.pow(this.x - otroPunto.x, 2) + Math.pow(this.y - otroPunto.y, 2));
-        }
+    public Punto2D(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
+    public double distancia(Punto2D otroPunto) {
+        return Math.sqrt(Math.pow(this.x - otroPunto.x, 2) + Math.pow(this.y - otroPunto.y, 2));
+    }
+}
+
+class EstacionBase {
+    int idEstacion;
+    Punto2D ubicacion;
+
+    public EstacionBase(int idEstacion, Punto2D ubicacion) {
+        this.idEstacion = idEstacion;
+        this.ubicacion = ubicacion;
+    }
+}
+
+class Persona {
+    int idPersona;
+    Punto2D ubicacion;
+    EstacionBase estacionAsociada;
+
+    public Persona(int idPersona, Punto2D ubicacion) {
+        this.idPersona = idPersona;
+        this.ubicacion = ubicacion;
+        this.estacionAsociada = null;
+    }
 }
